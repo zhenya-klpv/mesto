c46667fef5d32fa19c5c907f5e04bdf1f9668ab3
Eugene Kolpakov <kolpakov.e@icloud.com>
2020-08-19 19:49:46 -0400

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: { main: './src/pages/index.js' },
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
+  entry: {
+    main: './src/pages/index.js'
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+
+  }
 }
