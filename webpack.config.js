81088fc241a1016c54bb4d33069aa11acfc5b538
Eugene Kolpakov <kolpakov.e@icloud.com>
2020-08-19 21:57:21 -0400

    cssnano autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,62 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/pages/index.js'
-  },
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  devServer: {
-    port: 3000
-  },
-  module: {
+    entry: {
+      main: './src/pages/index.js'
+    },
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js'
+    },
+    devServer: {
+      port: 3000
+    },
+    module: {
     rules: [
       {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
+        test: /\.js$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }]
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        },
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1}
+             },
+            'postcss-loader'
+        ],
+      },
 
-  }
-}
+
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        loader: 'file-loader?name=./images/[name].[ext]',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]',
+      },
+    ]
+  },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: 'src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+      ]
+    }
